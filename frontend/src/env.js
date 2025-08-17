6dbbdb37c8bf34a4b449c5ed5937633af224afc5
zvitambo <tjindu1@gmail.com>
2025-08-17 13:16:41 +0200

    added Better Auth

--- frontend/src/env.js
+++ frontend/src/env.js
@@ -11,6 +11,20 @@ export const env = createEnv({
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
+    AWS_ACCESS_KEY_ID: z.string(),
+    AWS_SECRET_ACCESS_KEY: z.string(),
+    AWS_REGION: z.string(),
+    S3_BUCKET_NAME: z.string(),
+
+    GENERATE_FROM_DESCRIPTION: z.string(),
+    GENERATE_FROM_DESCRIBED_LYRICS: z.string(),
+    GENERATE_WITH__LYRICS: z.string(),
+
+    MODAL_KEY: z.string(),
+    MODAL_SECRET: z.string(),
+
+    BETTER_AUTH_SECRET: z.string(),
+    BETTER_AUTH_URL: z.string(),
   },
 
   /**
@@ -29,6 +43,20 @@ export const env = createEnv({
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
+    AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+    AWS_REGION: process.env.AWS_REGION,
+    S3_BUCKET_NAME: process.env.S3_BUCKET_NAME,
+
+    GENERATE_FROM_DESCRIPTION: process.env.GENERATE_FROM_DESCRIPTION,
+    GENERATE_FROM_DESCRIBED_LYRICS: process.env.GENERATE_FROM_DESCRIBED_LYRICS,
+    GENERATE_WITH__LYRICS: process.env.GENERATE_WITH__LYRICS,
+
+    MODAL_KEY: process.env.MODAL_KEY,
+    MODAL_SECRET: process.env.MODAL_SECRET,
+
+    BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
+    BETTER_AUTH_URL: process.env.BETTER_AUTH_URL,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
